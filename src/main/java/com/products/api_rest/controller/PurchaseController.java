958a7e4a4920d9660f5a655085cdbd5f67fc83c0
gmachadom <120364460+gmachadom@users.noreply.github.com>
2024-07-29 02:07:29 -0400

    Consertando criacao de compras

---
+++ src/main/java/com/products/api_rest/controller/PurchaseController.java
@@ -0,0 +1,29 @@
+package com.products.api_rest.controller;
+
+import com.products.api_rest.models.Purchase;
+import com.products.api_rest.repository.PurchaseRepository;
+import com.products.api_rest.service.PurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/purchases")
+public class PurchaseController {
+    @Autowired
+    private PurchaseRepository purchaseRepository;
+
+    @Autowired
+    private PurchaseService purchaseService;
+
+    @GetMapping()
+    public ResponseEntity<?> getAllPurchases() {
+        return purchaseService.getAllPurchases();
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> addPurchase(@RequestBody Purchase purchase) {
+        return purchaseService.addPurchase(purchase);
+    }
+
+}
